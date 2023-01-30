373565d5092a827a50470e78ee2204a9f1e7e7e9
Wyback <baptiste.liscouet@gmail.com>
2023-01-30 09:45:23 +0100

    change reducer type

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -3,8 +3,8 @@ import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { LaunchListState } from './app.interface';
 import { AppReducer, reducerLaunch } from './reducers/reducer-launchs';
 
-export const reducers: ActionReducerMap<LaunchListState> = {
+export const reducers: ActionReducerMap<{ launchs: LaunchListState }> = {
     launchs: AppReducer
   };
 
-  export const metaReducers: MetaReducer<LaunchListState>[] = [];
+  export const metaReducers: MetaReducer<{ launchs: LaunchListState }>[] = [];
